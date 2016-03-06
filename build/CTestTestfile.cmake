438676759ed226e6bac76433ddfa4a5d01868fc9
Dominik Schreiber <dompas.schreiber@t-online.de>
2016-03-06 16:09:43 +0100

    Changed config location and added instructions in README and INSTALL.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominik/workspace/krunner-symbols/krunner-symbols
-# Build directory: /home/dominik/workspace/krunner-symbols/krunner-symbols/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
