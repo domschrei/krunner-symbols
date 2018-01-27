e3cdfb8370728d3c15117199e5c1f8e831bbe0f2
Dominik Schreiber <mail@dominikschreiber.de>
2018-01-27 16:33:02 +0100

    Tidy up install script

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Caution - This script is not guaranteed to work properly, if any complications occur. 
-# In case of doubt, execute these steps one after one and watch the output.
-
+# Exit immediately if something fails
 set -e
 
+# Setup build directory
 mkdir -p build
 cd build
 
+# Build the plugin
 cmake .. -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DQT_PLUGIN_INSTALL_DIR=`kf5-config --qt-plugins`
 make 
+# Install the plugin (root access because it has to write into /usr)
 sudo make install
 
-set +e
-
 # Restart krunner for the changes to take effect
-kquitapp5 krunner 2> /dev/null
-kstart5 --windowclass krunner krunner > /dev/null 2>&1 & 
+kquitapp5 krunner 2> /dev/null || echo "No krunner instance has been running."
+kstart5 --windowclass krunner krunner > /dev/null 2>&1 &
