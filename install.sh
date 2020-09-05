b862908779f9cbf1d6058f8f05f8b471f7d027ba
Dominik Schreiber <mail@dominikschreiber.de>
2020-09-05 12:07:18 +0200

    Update deprecated variable name QT_PLUGIN_INSTALL_DIR -> KDE_INSTALL_QTPLUGINDIR

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p build
 cd build
 
 # Build the plugin
-cmake .. -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DQT_PLUGIN_INSTALL_DIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DKDE_INSTALL_QTPLUGINDIR=`kf5-config --qt-plugins` -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 # Install the plugin (root access because it has to write into /usr)
 sudo make install
